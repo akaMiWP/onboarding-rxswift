b50e4696e675579352a427ed785bd8eff813f6f4
akaMiWP <bangkokappdevelopment@gmail.com>
2024-12-06 22:50:26 +0700

    Finished 1st challenge

--- Subjects/Sources/main.swift
+++ Subjects/Sources/main.swift
@@ -167,3 +167,43 @@ example(of: "BehaviorRelay") {
     
     print(relay.value)
 }
+
+// MARK: - Challenges
+
+/// 1st challenge
+example(of: "Create a blackjack card dealer using a publish subject") {
+    
+    let disposeBag = DisposeBag()
+    
+    let dealtHand = PublishSubject<[(String, Int)]>()
+    
+    func deal(_ cardCount: UInt) {
+        var deck = cards
+        var cardsRemaining = deck.count
+        var hand = [(String, Int)]()
+        
+        for _ in 0..<cardCount {
+            let randomIndex = Int.random(in: 0..<cardsRemaining)
+            hand.append(deck[randomIndex])
+            deck.remove(at: randomIndex)
+            cardsRemaining -= 1
+        }
+        
+        // Add code to update dealtHand here
+        dealtHand.onNext(hand)
+    }
+    
+    // Add subscription to dealtHand here
+    dealtHand
+        .subscribe(
+            onNext: {
+                print(cardString(for: $0), points(for: $0))
+            },
+            onError: {
+                print($0)
+            }
+        )
+        .disposed(by: disposeBag)
+    
+    deal(3)
+}
