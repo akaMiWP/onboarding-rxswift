9301b862b5862b92be2a0e7ae0f5a1f60a56bc20
akaMiWP <bangkokappdevelopment@gmail.com>
2024-12-12 17:00:02 +0700

    Commited main.swift file

--- Schedulers/Sources/main.swift
+++ Schedulers/Sources/main.swift
@@ -17,5 +17,23 @@ animal
 
 // Start coding here
 
+let fruit = Observable<String>.create { observer in
+  observer.onNext("[apple]")
+  sleep(2)
+  observer.onNext("[pineapple]")
+  sleep(2)
+  observer.onNext("[strawberry]")
+  return Disposables.create()
+}
+
+
+fruit
+    .subscribeOn(globalScheduler)
+    .dump()
+    .observeOn(MainScheduler.instance)
+    .dumpingSubscription()
+    .disposed(by: bag)
+
 
 RunLoop.main.run(until: Date(timeIntervalSinceNow: 13))
+
