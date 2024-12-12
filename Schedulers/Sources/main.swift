9122bd0643fd394c89c0ed36245196df3454bd30
akaMiWP <bangkokappdevelopment@gmail.com>
2024-12-12 11:02:16 +0700

    Initialized Schedulers project

---
+++ Schedulers/Sources/main.swift
@@ -0,0 +1,21 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+import Foundation
+import RxSwift
+
+print("\n\n\n===== Schedulers =====\n")
+
+let globalScheduler = ConcurrentDispatchQueueScheduler(queue: DispatchQueue.global())
+let bag = DisposeBag()
+let animal = BehaviorSubject(value: "[dog]")
+
+animal
+  .dump()
+  .dumpingSubscription()
+  .disposed(by: bag)
+
+// Start coding here
+
+
+RunLoop.main.run(until: Date(timeIntervalSinceNow: 13))
