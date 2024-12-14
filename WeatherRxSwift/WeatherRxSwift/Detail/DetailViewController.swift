e9c683989dc3b2272697f4e026eeab654ebb19d7
akaMiWP <bangkokappdevelopment@gmail.com>
2024-12-14 16:35:38 +0700

    Integrated UI components on DetailViewController

--- WeatherRxSwift/WeatherRxSwift/Detail/DetailViewController.swift
+++ WeatherRxSwift/WeatherRxSwift/Detail/DetailViewController.swift
@@ -6,6 +6,13 @@ import UIKit
 
 final class DetailViewController: UIViewController {
     
+    private let mainLabel: UILabel = .init()
+    private let descriptionLabel: UILabel = .init()
+    private let latitudeLabel: UILabel = .init()
+    private let longtitudeLabel: UILabel = .init()
+    private let tempLabel: UILabel = .init()
+    private let feelLikesLabel: UILabel = .init()
+    
     private let viewModel: DetailViewModel
     private let disposeBag: DisposeBag = .init()
     
@@ -35,12 +42,79 @@ final class DetailViewController: UIViewController {
 // MARK: - Private
 private extension DetailViewController {
     func configureUI() {
+        [mainLabel, descriptionLabel, latitudeLabel, longtitudeLabel, tempLabel, feelLikesLabel]
+            .forEach { view.addSubview($0) }
+        
+        mainLabel.snp.makeConstraints {
+            $0.top.equalTo(view.snp.topMargin)
+            $0.leading.trailing.equalToSuperview().inset(12)
+        }
+        
+        descriptionLabel.snp.makeConstraints {
+            $0.top.equalTo(mainLabel.snp.bottom).offset(12)
+            $0.leading.trailing.equalToSuperview().inset(12)
+        }
+        
+        latitudeLabel.snp.makeConstraints {
+            $0.top.equalTo(descriptionLabel.snp.bottom).offset(12)
+            $0.leading.trailing.equalToSuperview().inset(12)
+        }
+        
+        longtitudeLabel.snp.makeConstraints {
+            $0.top.equalTo(latitudeLabel.snp.bottom).offset(12)
+            $0.leading.trailing.equalToSuperview().inset(12)
+        }
+        
+        tempLabel.snp.makeConstraints {
+            $0.top.equalTo(longtitudeLabel.snp.bottom).offset(12)
+            $0.leading.trailing.equalToSuperview().inset(12)
+        }
+        
+        feelLikesLabel.snp.makeConstraints {
+            $0.top.equalTo(tempLabel.snp.bottom).offset(12)
+            $0.leading.trailing.equalToSuperview().inset(12)
+        }
+        
         view.backgroundColor = .white
     }
     
     func bindUI() {
-        viewModel.modelDriver.map { $0.name }
+        viewModel.modelDriver.map(\.name)
             .drive(self.navigationItem.rx.title)
+            .disposed(by: disposeBag)
+        
+        let weatherDriver = viewModel.modelDriver.compactMap { $0.weather.first }
+        weatherDriver
+            .map(\.main)
+            .map { "Main Type of Weather: \($0)" }
+            .drive(mainLabel.rx.text)
+            .disposed(by: disposeBag)
+        weatherDriver
+            .map(\.description)
+            .map { "Weather description: \($0)" }
+            .drive(descriptionLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        let coordinateDriver = viewModel.modelDriver.map(\.coordinate)
+        coordinateDriver
+            .map(\.lat)
+            .map { "Latitude: \($0)" }
+            .drive(latitudeLabel.rx.text)
+            .disposed(by: disposeBag)
+        coordinateDriver
+            .map(\.lon)
+            .map { "Longtitude: \($0)" }
+            .drive(longtitudeLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        let detailsDriver = viewModel.modelDriver.map(\.details)
+        detailsDriver.map(\.temp)
+            .map { "Temperature is: \($0)" }
+            .drive(tempLabel.rx.text)
+            .disposed(by: disposeBag)
+        detailsDriver.map(\.feelsLike)
+            .map { "Temperature Feel Like: \($0)" }
+            .drive(feelLikesLabel.rx.text)
             .disposed(by: disposeBag)
     }
 }
